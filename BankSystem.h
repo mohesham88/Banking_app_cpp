d5ffd502834bf29a00795c7a894ce040a1ea3bbd
AdhamMahmoud1 <101528034+AdhamMahmoud1@users.noreply.github.com>
2022-11-07 23:00:36 +0200

    Add files via upload

---
+++ BankSystem.h
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BankingApplication {
+    private:
+        
+    public:
+
+};
+class BankAccount{
+    private:
+        int accountID;
+        int balance;
+        Client c;
+    public:
+
+
+};
+
+class Client {
+    private:
+        string name;
+        string phone;
+        string address;
+        BankAccount ba;
+};
+
+class  SavingsBankAccount: public BankAccount{
+    public:
+
+};
+
+
+
