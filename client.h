f221dd22ce045a2b5f587f6ae2c9531b4397ccc1
SohaAshraff <102482621+SohaAshraff@users.noreply.github.com>
2022-11-11 21:42:25 +0200

    Update client.h
    Client.h

--- client.h
+++ client.h
@@ -1,13 +1,48 @@
-#include "banking_application.h"
-
-class Client {
+#pragma once
+#include<iostream>
+#include <string>
+#include "BankAccount.h"
+using namespace std;
+class Client
+{
 private:
-        string name;
-        string phone;
-        string address;
-        Bank_account bank_account;
+	string name;
+	string address;
+	int phone;
+    string Id;
+    double balance;
+    static int counter;
+	BankAccount* client_account;
+    bool checkname(string name) {
+        for (int i = 0; i < name.size(); i++) {
+            if (!isalpha(name[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
 public:
-    Client(string name,string phone,string address);    
+    
+    
+    void setId();
+    void setBalance(double balance);
+    void setName(string name);
+    void setAddress(string address);
+    void setNumber(int phone);
+    void set_account_Id();
+    void set_balance(double b);
+    double getbalance();
+    string getid();
+    
 
+    Client(string name = " ", string address = " ", int phone = 0) {
+        string id;
+        setName(name);
+        setAddress(address);
+        setNumber(phone);
+        setId();
+    }
 
+ 
 };
+
