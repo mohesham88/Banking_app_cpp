ae1efdde503e5d254568a7379698cd814f2cb9cf
SohaAshraff <102482621+SohaAshraff@users.noreply.github.com>
2022-11-11 21:45:42 +0200

    Update savings_bank_account.cpp
    savingaccount

--- savings_bank_account.cpp
+++ savings_bank_account.cpp
@@ -1,1 +1,26 @@
-#include "BankSystem.h"
+#include "savingaccount.h"
+int SavingAcount::minimumBalance(double amount) {
+	if (amount < 100)
+	{
+		cout << "sorry, you have to make deposits that are not less than 100 a time." << endl;
+		return 0;
+
+	}
+	else
+	{
+		BankAccount::setBalance(amount);
+		return 1;
+	}
+}
+
+void SavingAcount::deposit(double amoun) {
+	int a;
+	a = minimumBalance(amoun);
+	if (a == 1) {
+		BankAccount::deposit(amoun);
+	}
+}
+void SavingAcount::withdraw(double amoun)
+{
+	BankAccount::withdraw(amoun);
+}
