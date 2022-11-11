c316d022306146842c8bac554f3b003173beb648
SohaAshraff <102482621+SohaAshraff@users.noreply.github.com>
2022-11-11 21:43:39 +0200

    Update bank_account.cpp
    bankaccount

--- bank_account.cpp
+++ bank_account.cpp
@@ -1,36 +1,13 @@
-#include "bank_account.h"
-
-Bank_account::Bank_account(int account_id,double balance){
-        this->id = account_id;
-        this->balance = balance;
+#include "BankAccount.h"
+int BankAccount::deposit(double amount) {
+	setBalance(balance += amount);
+	return this->balance;
 }
-
-Bank_account::Bank_account(){
-    this->balance = 0;
+int BankAccount::withdraw(double amount) {
+	if (balance < amount) {
+		cout << " This is more than what you can withdraw" << endl;
+	}
+	else
+		setBalance(balance -= amount);
+	return this->balance;
 }
-
-void Bank_account::set_id(int id){
-    this->id = id;
-}
-void Bank_account::set_balance(double balance){
-    this->balance = balance;
-}
-int Bank_account::get_id(){
-    return id;
-}
-double Bank_account::get_balance(){
-    return balance;
-}
-
-void Bank_account::withdraw(double money){
-    if(money <= balance){
-        balance -= money;
-    }
-    else{
-        cout << "Sorry. This is more than what you can withdraw.\n";
-    }
-}
-void Bank_account::deposit(double money){
-    balance += money;
-}
-
